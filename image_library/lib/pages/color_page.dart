87cdc55609c496d88a068d1c034e2ec4cda1caca
Juan Antonio <ovallejuan98@gmail.com>
2020-03-24 15:16:07 -0600

    Se agrego un boton para obtener la notacion en Munsell

--- image_library/lib/pages/color_page.dart
+++ image_library/lib/pages/color_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_library/image.dart';
+import 'package:image_library/munsell.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ColorPage extends StatefulWidget {
@@ -13,6 +14,8 @@ class ColorPage extends StatefulWidget {
 class _ColorPageState extends State<ColorPage> {
   File foto;
   Color col = Colors.red;
+  Munsell m = new Munsell();
+  String notacionMunsell = 'Munsell';
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +39,11 @@ class _ColorPageState extends State<ColorPage> {
                   child: image(),
                 ),
                 SizedBox(height: 10.0,),
-                _crearBoton()
+                _crearBoton(),
+                SizedBox(height: 10.0,),
+                _crearBotonMunsell(),
+                SizedBox(height: 20.0,),
+                Text(notacionMunsell),
               ],
             ),
           ),
@@ -51,7 +58,6 @@ class _ColorPageState extends State<ColorPage> {
     );
     Color c = imageColor(foto);
     col = Color.fromRGBO(c.red, c.green, c.blue, 1.0);
-    print(col);
     setState(() {});
   }
 
@@ -80,5 +86,43 @@ class _ColorPageState extends State<ColorPage> {
     );
   }
 
+  Widget _crearBotonMunsell() {
+    return StreamBuilder(
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        return RaisedButton(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+            child: Text('Munsell'),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(19.0)
+          ),
+          elevation: 5.0,
+          onPressed: (){
+            String rgb = _colorToString();
+            notacionMunsell = m.getColorMunsell(rgb);
+            setState(() {});
+            print(notacionMunsell);
+          },
+        );
+      },
+    );
+  }
+
+  //Convierte el RGB en String; ejemplo '000031045'
+  String _colorToString(){
+    String r = _completarString(col.red.toString());
+    String g = _completarString(col.green.toString());
+    String b = _completarString(col.blue.toString());
+    return '$r$g$b';
+  }
+
+  //Completa el string a tres caracateresticas '1' => '001'
+  String _completarString(String x) {
+    if(x.length == 3) return x;
+    else if(x.length == 2) return '0$x';
+    else return '00$x';
+  }
 
 }
