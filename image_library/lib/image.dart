3b82d3dbf55bc7be20d53a0b044e4e7f60fa2274
Juan Antonio <ovallejuan98@gmail.com>
2020-03-24 15:09:09 -0600

    Se cambio el tamaño de copiar la imagen

--- image_library/lib/image.dart
+++ image_library/lib/image.dart
@@ -10,8 +10,8 @@ Map<Color, int> map;
 
 Color imageColor(File foto){
   img.Image im = img.decodeImage(foto.readAsBytesSync());
-//  im = img.copyResize(im, width: (im.width*0.10).toInt(), height: (im.height*0.10).toInt());
-  im = img.copyResize(im, width: 200, height: 200);
+  im = img.copyResize(im, width: (im.width*0.10).toInt(), height: (im.height*0.10).toInt());
+//  im = img.copyResize(im, width: 200, height: 200);
   
   for (var i = 0; i < im.width; i++) {
     for (var j = 0; j < im.height; j++) {
@@ -29,9 +29,5 @@ Color imageColor(File foto){
   blueP = (blueP/(im.width*im.height)).round();
   c = Color.fromRGBO(blueP, greenP, redP, 1.0);
   colorFinal = c;
-  print('-------------------------------------------------');
-  print('$c RGB($redP, $greenP, $blueP)');
-  print('$colorFinal  ${colorFinal.red} ${colorFinal.green} ${colorFinal.blue}');
-  print('-------------------------------------------------');
   return colorFinal;
 }
